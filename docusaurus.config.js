c2a28223d173a91aca6bd5ba0b9ba048c85b2a0f
Kelz <makamanekj@gmail.com>
2023-07-28 17:41:18 +0200

    feat(ANNULUS-264): adds landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,52 +1,52 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Topl',
-  tagline: '',
-  url: 'http://docs.topl.co',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/cropped-topl_favicon_new-32x32.png',
+  title: "Topl",
+  tagline: "",
+  url: "http://docs.topl.co",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/cropped-topl_favicon_new-32x32.png",
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'topl', // Usually your GitHub org/user name.
-  projectName: 'knowledge-base', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "topl", // Usually your GitHub org/user name.
+  projectName: "knowledge-base", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/Topl/knowledge-base/edit/main/',
+          editUrl: "https://github.com/Topl/knowledge-base/edit/main/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -54,11 +54,11 @@ const config = {
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
@@ -66,67 +66,67 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'Topl',
-          src: 'img/topl_logo_horizontal.svg',
-          srcDark: 'img/topl_logo_white.svg',
-          href: 'https://topl.co',
-          target: '_self',
+          alt: "Topl",
+          src: "img/topl_logo_white.svg",
+          srcDark: "img/topl_logo_white.svg",
+          href: "/",
+          target: "_self",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'About/welcome',
-            position: 'left',
-            label: 'About',
+            type: "doc",
+            docId: "About/welcome",
+            position: "left",
+            label: "About",
           },
           {
-            type: 'doc',
-            docId: 'Developers/Bifrost/contributing',
-            position: 'left',
-            label: 'Developers',
+            type: "doc",
+            docId: "Developers/Bifrost/contributing",
+            position: "left",
+            label: "Developers",
           },
           {
-            type: 'doc',
-            docId: 'Validators/Installing Your Node/install-docker',
-            position: 'left',
-            label: 'Validators',
+            type: "doc",
+            docId: "Validators/Installing Your Node/install-docker",
+            position: "left",
+            label: "Validators",
           },
           {
-            href: 'https://github.com/topl/knowledge-base',
-            position: 'right',
-            'aria-label': 'GitHub repository',
-            className: 'header-github-link',
+            href: "https://github.com/topl/knowledge-base",
+            position: "right",
+            "aria-label": "GitHub repository",
+            className: "header-github-link",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/nvaFy4HpUA',
+                label: "Discord",
+                href: "https://discord.gg/nvaFy4HpUA",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/topl_protocol',
+                label: "Twitter",
+                href: "https://twitter.com/topl_protocol",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                href: 'https://medium.com/topl-blog',
+                label: "Blog",
+                href: "https://medium.com/topl-blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/topl/knowledge-base',
+                label: "GitHub",
+                href: "https://github.com/topl/knowledge-base",
               },
             ],
           },
@@ -139,12 +139,12 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: 'HO3PUAD52H',
+        appId: "HO3PUAD52H",
 
         // Public API key: it is safe to commit it
-        apiKey: '6ceefa2af5b17ceb3aa6623152f5532b',
+        apiKey: "6ceefa2af5b17ceb3aa6623152f5532b",
 
-        indexName: 'topl',
+        indexName: "topl",
 
         // Optional
         contextualSearch: true,
